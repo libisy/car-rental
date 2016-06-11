36295bfd59d0d5fbbd9d88385077f4b58fb54ec3
Sagar Kulung <libisy14@gmail.com>
2016-06-11 15:33:12 +0545

    remove unwanted change

--- components/header.php
+++ components/header.php
@@ -4,5 +4,3 @@
 	<link href="public/css/style1.css" rel="stylesheet" type="text/css" />
 	<link rel="stylesheet" href="public/css/skeleton-2.0.4/css/skeleton.css" title="no title" charset="utf-8">
 	<link rel="stylesheet" href="public/css/skeleton-2.0.4/css/normalize.css" title="no title" charset="utf-8">
-
-	<!-- some change  -->
